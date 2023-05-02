9021b7a56bf1dff9f3f21e57543b024a6d5d924e
LenaBystrykh <lena.bystryh@mail.ru>
2023-05-02 20:32:26 +0500

    trying to add apollo part 1

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import axios from 'axios'
+// import axios from 'axios'
 import messages from '@/i18n'
+import { apolloClient } from './vue-apollo'
 
 const i18n = createI18n({
     locale: 'ru-RU',
@@ -13,8 +15,15 @@ const i18n = createI18n({
     messages,
 })
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
-const app = createApp(App)
+// axios.defaults.baseURL = 'http://127.0.0.1:8000'
+const app = createApp({
+    setup () {
+        provide(DefaultApolloClient, apolloClient)
+    },
+  
+    render: () => h(App),
+})
 
 app.use(i18n)
-app.use(store).use(router, axios).mount('#app')
+// app.use(store).use(router, axios).mount('#app')
+app.use(store).use(router).mount('#app')
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -2,7 +2,7 @@
     <div class="initial-page">
         <div class="initial-form">
             <h1 class="initial-title">{{ $t('signUp') }}</h1>
-            <form @submit.prevent="submitForm">
+            <form @submit.prevent="create_user">
                 <div class="initial-field">
                     <label class="initial-field-label">{{ $t('email') }}</label>
                     <input type="email" name="username" class="initial-input" v-model="username" placeholder="example@mail.ru">
@@ -33,6 +33,20 @@
 <script>
 import axios from 'axios'
 
+const UserCreate = gql`mutation createUser($username:String, $surname: String, $name:String, $password: String, $role:String) {
+      createUser(username: $username, surname: $surname, name: $name, password: $password, role: $role) {
+        user {
+            id
+            username
+            surname
+            name
+            password
+            role
+        }
+        ok
+    }
+ }`
+
 export default {
     name: 'SignUp',
     data() {
@@ -40,109 +54,156 @@ export default {
             username: '',
             surname: '',
             name: '',
-            patronymic: '',
             password: '',
-            repeat: '',
             student: true,
             errors: []
         }
     },
     methods: {
-        async submitForm(e) {
-            const formData = {
-                username: this.username,
-                surname: this.surname,
-                name: this.name,
-                patronymic: this.patronymic,
-                password: this.password,
-                student: this.student
-            }
+        // async submitForm(e) {
+        //     const formData = {
+        //         username: this.username,
+        //         surname: this.surname,
+        //         name: this.name,
+        //         patronymic: this.patronymic,
+        //         password: this.password,
+        //         student: this.student
+        //     }
 
-            const client = {
-                email: this.username, 
-                surname: this.surname,
-                name: this.name,
-                patronymic: this.patronymic,
-                role: this.student === true ? 'Студент' : 'Преподаватель'
-            }
+        //     const client = {
+        //         email: this.username, 
+        //         surname: this.surname,
+        //         name: this.name,
+        //         patronymic: this.patronymic,
+        //         role: this.student === true ? 'Студент' : 'Преподаватель'
+        //     }
 
-            await axios
-                .post('/api/v1/users/', formData)
-                .then(response => {
-                    console.log(response)
-                })
-                .catch(error => {
-                    if (error.response) {
-                        for (const property in error.response.data) {
-                            this.errors.push(`${property}: ${error.response.data[property]}`)
-                        }
-                        console.log(JSON.stringify(error.response.data))
-                    } else if (error.message) {
-                        console.log(JSON.stringify(error.message))
-                    } else {
-                        console.log(JSON.stringify(error))
-                    }
-                })
+        //     await axios
+        //         .post('/api/v1/users/', formData)
+        //         .then(response => {
+        //             console.log(response)
+        //         })
+        //         .catch(error => {
+        //             if (error.response) {
+        //                 for (const property in error.response.data) {
+        //                     this.errors.push(`${property}: ${error.response.data[property]}`)
+        //                 }
+        //                 console.log(JSON.stringify(error.response.data))
+        //             } else if (error.message) {
+        //                 console.log(JSON.stringify(error.message))
+        //             } else {
+        //                 console.log(JSON.stringify(error))
+        //             }
+        //         })
             
-            axios.defaults.headers.common["Authorization"] = ""
+        //     axios.defaults.headers.common["Authorization"] = ""
 
-            localStorage.removeItem('token')
+        //     localStorage.removeItem('token')
 
-            const logInData = {
-                username: this.username,
-                password: this.password
-            }
+        //     const logInData = {
+        //         username: this.username,
+        //         password: this.password
+        //     }
 
-            await axios
-                .post('/api/v1/token/login/', logInData)
-                .then(response => {
-                    const token = response.data.auth_token
-                    this.$store.commit('setToken', token)
-                    axios.defaults.headers.common["Authorization"] = "Token " + token
-                    localStorage.setItem('token', token)
-                })
-                .catch(error => {
-                    if (error.response) {
-                        for (const property in error.response.data) {
-                            this.errors.push(`${property}: ${error.response.data[property]}`)
-                        }
-                        console.log(JSON.stringify(error.response.data))
-                    } else if (error.message) {
-                        console.log(JSON.stringify(error.message))
-                    } else {
-                        console.log(JSON.stringify(error))
-                    }
-                })
+        //     await axios
+        //         .post('/api/v1/token/login/', logInData)
+        //         .then(response => {
+        //             const token = response.data.auth_token
+        //             this.$store.commit('setToken', token)
+        //             axios.defaults.headers.common["Authorization"] = "Token " + token
+        //             localStorage.setItem('token', token)
+        //         })
+        //         .catch(error => {
+        //             if (error.response) {
+        //                 for (const property in error.response.data) {
+        //                     this.errors.push(`${property}: ${error.response.data[property]}`)
+        //                 }
+        //                 console.log(JSON.stringify(error.response.data))
+        //             } else if (error.message) {
+        //                 console.log(JSON.stringify(error.message))
+        //             } else {
+        //                 console.log(JSON.stringify(error))
+        //             }
+        //         })
                 
-            await axios
-                .get('/api/v1/users/me')
-                .then(response => {
-                    this.$store.commit('setUser', {'username': response.data.username, 'id': response.data.id})                    
-                    localStorage.setItem('username', response.data.username)
-                    localStorage.setItem('userid', response.data.id)
-                })
-                .catch(error => {
-                    console.log(JSON.stringify(error))
-                })
+        //     await axios
+        //         .get('/api/v1/users/me')
+        //         .then(response => {
+        //             this.$store.commit('setUser', {'username': response.data.username, 'id': response.data.id})                    
+        //             localStorage.setItem('username', response.data.username)
+        //             localStorage.setItem('userid', response.data.id)
+        //         })
+        //         .catch(error => {
+        //             console.log(JSON.stringify(error))
+        //         })
 
-            axios
-                .post('/api/v1/clients/', client)
-                .then(response => {
-                    toast({
-                        message: 'The client has been created',
-                        type: 'is-success',
-                        dismissible: true,
-                        pauseOnHover: true,
-                        duration: 2000,
-                        position: 'top-right',
-                    })
+        //     axios
+        //         .post('/api/v1/clients/', client)
+        //         .then(response => {
+        //             toast({
+        //                 message: 'The client has been created',
+        //                 type: 'is-success',
+        //                 dismissible: true,
+        //                 pauseOnHover: true,
+        //                 duration: 2000,
+        //                 position: 'top-right',
+        //             })
 
-                })
-                .catch(error => {
-                    console.log(JSON.stringify(error))
-                })
+        //         })
+        //         .catch(error => {
+        //             console.log(JSON.stringify(error))
+        //         })
             
-            this.$router.push('/dashboard/articles')
+        //     this.$router.push('/dashboard/articles')
+        // },
+
+        async create_user() {
+            const email = this.username
+            const surname = this.surname
+            const name = this.name
+            const password = this.password
+            const role = this.student === true ? 'student' : 'teacher'
+
+            // Call to the graphql mutation
+            let data = await this.$apollo.mutate({
+                // Query
+                mutation: UserCreate,
+                // Parameters
+                variables: {
+                    username: email,
+                    surname: surname,
+                    name: name,
+                    password: password,
+                    role: role,
+                },
+                update: (store, { data: { createUser } }) => {
+                    // Add to All users list
+                    const data = store.readQuery({ query: UserQuery })
+                    data.users.push(createUser.user)
+                    store.writeQuery({ query: UserQuery, data })
+                },
+                // optimisticResponse: {
+                //       __typename: 'Mutation',
+                //       createTask: {
+                //         __typename: 'CreateTask',
+                //         task: {
+                //           __typename: "TaskType",
+                //           id: -1,
+                //           isDone: false,
+                //           name: name,
+                //           description: description                 
+                //         },
+                //         ok: false
+                //       }
+                // },
+            })
+            var t = data.data.createUser.user
+            console.log('Added: ' , t)
+            this.username = ''
+            this.surname = ''
+            this.name = ''
+            this.password = ''
+            this.student = true
         }
     }
 }
